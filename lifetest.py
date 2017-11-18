bf3402aaf71753e03d5215297fd8fe3e9b946bda
KonWal <33419410+KonWal@users.noreply.github.com>
2017-11-18 11:58:06 +0000

    Addded unit test framework

--- lifetest.py
+++ lifetest.py
@@ -1,8 +1,14 @@
-def test_cell_update():
-    assert(cell_update(1, 4), 1)
+import unittest
+
+class test_cell_updates(unittest.TestCase):
+    
+    def test_cell_update_overpop(self):
+        assert(cell_update(1, 4), 0)
 
 def cell_update(cell, neighbours):
     return 0
 
-test_cell_update()
+if __name__ == "__main__":
+
+    unittest.main()
     
