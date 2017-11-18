1917db4a3cb60092b95bbc9dab532e1a880208e4
KonWal <33419410+KonWal@users.noreply.github.com>
2017-11-18 12:04:58 +0000

    three neighbours

--- lifetest.py
+++ lifetest.py
@@ -8,11 +8,14 @@ class test_cell_updates(unittest.TestCase):
     def test_cell_update_birth(self):
         self.assertEqual(cell_update(0,3), 1)
 
+    def test_cell_update(self):
+        self.assertEqual(cell_update(1,3), 1)
+
 def cell_update(cell, neighbours):
-    if cell == 1:
-        return 0
-    else:
+    if neighbours == 3:
         return 1
+    else:
+        return 0
 
 if __name__ == "__main__":
 
