442d9fb3acb00be992ffb4a0d51b9bae194b4a6a
KonWal <33419410+KonWal@users.noreply.github.com>
2017-11-18 12:17:32 +0000

    all cases?

--- lifetest.py
+++ lifetest.py
@@ -14,12 +14,18 @@ class test_cell_updates(unittest.TestCase):
     def test_cell_update_underpop(self):
         self.assertEqual(cell_update(1,1),0)
 
+    def test_cell_update_nopop(self):
+        self.assertEqual(cell_update(0,2),0)
 
 def cell_update(cell, neighbours):
-    if neighbours == 3 or neighbours == 2:
+    if neighbours == 3:
         return 1
+    elif neighbours == 2:
+        return cell
     else:
         return 0
+
+
 
 if __name__ == "__main__":
 
